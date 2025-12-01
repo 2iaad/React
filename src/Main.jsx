672ef28d817e20f925cff1949d3a08154dfc71c8
2iaad <ziadderfoufi77@gmail.com>
2025-12-01 05:05:12 +0100

    [practicing useState()]

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,66 +1,27 @@
+import Recommendation from './Recommendation'
 import { useState } from 'react'
 
 export default function Main() {
 
     const [ingredients, setIngredients] = useState([])
+    const [isRecipe, setIsRecipe] = useState(false)
 
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ))
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function handleSubmit(formData) {
         const newIngredient = formData.get("ingredient")
-
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
-    return (
-        <main>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
-                <input
-                    type="text"
-                    placeholder="e.g. oregano"
-                    aria-label="Add ingredient"
-                    name="ingredient"
-                />
-                <button>Add ingredient</button>
-            </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
-        </main>
-    )
-}
-
-
-
-
-
-
-
-
-
-/* 
-
-export default function Main() {
-
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
-
-    const ingredientsListItems = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ))
-
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
-        const newIngredient = formData.get("ingredient")
+    function handleRecipe() {
+        setIsRecipe(boolVar => !boolVar)
     }
-
+    
     return (
         <main>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
+            <form action={handleSubmit} className="add-ingredient-form">
                 <input
                     type="text"
                     placeholder="e.g. oregano"
@@ -69,11 +30,29 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+
+            <section>
+
+                {ingredients.length > 0 ? // conditional rendering
+                    <>
+                        <ul id='list'>
+                            <h2>Ingredients on hand:</h2>
+                            {ingredientsListItems}
+                        </ul>
+
+                        {ingredients.length > 2 &&  <div className='get-recipe-container'>
+                            <div className='get-recipe-inner-container'>
+                                <h3>Ready for a recipe?</h3>
+                                <p>Generate a recipe from your list of ingredients.</p>
+                            </div>
+                            <button onClick={handleRecipe} >Get a recipe</button>
+                        </div>}
+                    </>
+                    :
+                    <ul><h2>List is empty!</h2></ul>}
+            
+            </section>
+            <Recommendation isRecipe2={isRecipe}/>
         </main>
     )
 }
-
-*/
