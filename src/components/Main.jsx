cf52463e5e82ca457a6a38dc6d9483afb4c932fe
2iaad <ziadderfoufi77@gmail.com>
2025-12-06 05:30:27 +0100

    [working..]

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,32 @@
+import { useState } from 'react';
 import Number from './Number'
 
-let numbers = [
-    {   id: 1,  value: Math.floor(Math.random() * 100)  },
-    {   id: 2,  value: Math.floor(Math.random() * 100)  },
-    {   id: 3,  value: Math.floor(Math.random() * 100)  },
-    {   id: 4,  value: Math.floor(Math.random() * 100)  },
-    {   id: 5,  value: Math.floor(Math.random() * 100)  },
-    {   id: 6,  value: Math.floor(Math.random() * 100)  },
-    {   id: 7,  value: Math.floor(Math.random() * 100)  },
-    {   id: 8,  value: Math.floor(Math.random() * 100)  },
-    {   id: 9,  value: Math.floor(Math.random() * 100)  },
-    {   id: 10, value: Math.floor(Math.random() * 100)  }
-]
 
 export default function Main() {
-
-    const number = numbers.map(element => {
+    
+    const [numbers, setNumbers] = useState(
+    [{  id: 1,  value: Math.floor(Math.random() * 10)  },
+        {   id: 2,  isHeld: false,   value: Math.floor(Math.random() * 10)  },
+        {   id: 3,  isHeld: false,   value: Math.floor(Math.random() * 10)  },
+        {   id: 4,  isHeld: false,   value: Math.floor(Math.random() * 10)  },
+        {   id: 5,  isHeld: false,   value: Math.floor(Math.random() * 10)  },
+        {   id: 6,  isHeld: false,   value: Math.floor(Math.random() * 10)  },
+        {   id: 7,  isHeld: false,   value: Math.floor(Math.random() * 10)  },
+        {   id: 8,  isHeld: false,   value: Math.floor(Math.random() * 10)  },
+        {   id: 9,  isHeld: false,   value: Math.floor(Math.random() * 10)  },
+        {   id: 10, isHeld: false,   value: Math.floor(Math.random() * 10)  }]
+    );
+    
+/*
+    const [numbers, setNumbers] = useState(
+        Array.from( { length: 10 },
+        (_, i) => ({
+            id: i + 1,
+            value: Math.floor(Math.random() * 10)
+        }))
+    );
+*/
+    const numbersRender = numbers.map(element => {
         return (
             <Number
                 key={element.id}
@@ -24,13 +35,23 @@ export default function Main() {
         )
     })
 
+    function handleRoll() {
+
+        setNumbers(
+        numbers.map((n) => ({
+            ...n,
+            value: Math.floor(Math.random() * 10),
+        }))
+        );
+    }
+    
     return (
         <main>
             <div id="numbers">
-                {number}
+                {numbersRender}
             </div>
 
-            <button id='button'>Roll</button>
+            <button onClick={handleRoll} id='button'>Roll</button>
         </main>
     )
 }
